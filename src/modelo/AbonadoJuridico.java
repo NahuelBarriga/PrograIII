7e2bd3cac874b2772e38acf53c0a3ef103aee59b
NahuelBarriga <105519579+NahuelBarriga@users.noreply.github.com>
2023-04-30 16:28:53 -0300

    16:28

--- src/modelo/AbonadoJuridico.java
+++ src/modelo/AbonadoJuridico.java
@@ -3,15 +3,32 @@ package modelo;
 import java.util.Iterator;
 import java.util.Map.Entry;
 
+/**
+ * @author 
+ * Clase que representa un abonado de tipo juridico dentro del sistema
+ */
 public class AbonadoJuridico extends Abonado{
 
+	/**
+	 * Constructor de la clase
+	 * @param nombre: nombre del abonado
+	 * @param DNI: DNI del abonado
+	 * <b> pre: </b> nombre tiene que ser no vacio y no null <br> 
+	 * <b> pre: </b> DNI tiene que ser no vacio y no null <br> 
+	 * <b> post: </b> Crea un objeto de tipo abonado 
+	 */
 	public AbonadoJuridico(String nombre, String DNI) {
 		super(nombre, DNI);
 	}
 
+	/**
+	 *metodo que calcula el costo neto de todos los vervicios
+	 * <b> pre: </b> HashMap debe ser no nulo <br>  
+	 * <b> post: </b> retorna el costo neto de los servicios de un domicilio 
+	 */
 	@Override
-	public double getCostoServicios() {//Chequear esto, no se si esta bien????????????????????
-		Iterator<Entry<String,Servicio>> it = this.servicios.entrySet().iterator();	//Iterator del Set de HashMap
+	public double getCostoServicios() {
+		Iterator<Entry<String,Servicio>> it = this.servicios.entrySet().iterator();
 		double resultado = 0;
 		
 		if (this.servicios.size() > 3)
@@ -23,6 +40,11 @@ public class AbonadoJuridico extends Abonado{
 		return resultado;
 	}
 
+	/**
+	 *metodo que clona el abonado <br>
+	 *el metodo siempre va a lanzar excepcion 
+	 *<b> post: </b> lanza excepcion
+	 */
 	@Override
 	public Object clone() throws CloneNotSupportedException {
 		return new CloneNotSupportedException("Imposible clonar una persona juridica");
