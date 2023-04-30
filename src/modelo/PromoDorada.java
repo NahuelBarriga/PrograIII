342cf443e8b2d711ea7d648173f1b0dae2a1cf73
NahuelBarriga <105519579+NahuelBarriga@users.noreply.github.com>
2023-04-30 16:33:09 -0300

    16:33

--- src/modelo/PromoDorada.java
+++ src/modelo/PromoDorada.java
@@ -2,14 +2,29 @@ package modelo;
 
 import interfaces.IPromocion;
 
+/**
+ * @author 
+ * clase que representa la promocion dorada dentro del sistema
+ *
+ */
 public class PromoDorada implements IPromocion {
 
 
+	/**
+	 *aplica la pomocion dorada a un domicilio de tipo vivienda 
+	 *<b> pre: </> costoBruto debe ser positivo y no nulo 
+	 *<b> post: </> retorna el costo bruto despues de la promocion
+	 */
 	@Override
 	public double getPromoVivienda(double costoBruto) {
 		return costoBruto-1500;
 	}
 
+	/**
+	 *aplica la pomocion dorada a un domicionio de tipo comercio 
+	 *<b> pre: </> costoBruto debe ser positivo y no nulo 
+	 *<b> post: </> retorna el costo bruto despues de la promocion
+	 */
 	@Override
 	public double getPromoComercio(double costoBruto) {
 		return costoBruto-2500;
--- src/modelo/PromoPlatino.java
+++ src/modelo/PromoPlatino.java
@@ -2,13 +2,28 @@ package modelo;
 
 import interfaces.IPromocion;
 
+/**
+ * @author 
+ * clase que representa la promocion dorada dentro del sistema
+ *
+ */
 public class PromoPlatino implements IPromocion {
 
+	/**
+	 *aplica la pomocion dorada a un domicilio de tipo vivienda 
+	 *<b> pre: </> costoBruto debe ser positivo y no nulo 
+	 *<b> post: </> retorna el costo bruto despues de la promocion
+	 */
 	@Override
 	public double getPromoVivienda(double costoBruto) {
 		return costoBruto*0.7;
 	}
 
+	/**
+	 *aplica la pomocion dorada a un domicionio de tipo comercio 
+	 *<b> pre: </> costoBruto debe ser positivo y no nulo 
+	 *<b> post: </> retorna el costo bruto despues de la promocion
+	 */
 	@Override
 	public double getPromoComercio(double costoBruto) {
 		return costoBruto*0.65;
